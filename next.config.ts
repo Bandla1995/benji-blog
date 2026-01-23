d6051d65dc0b1b96d28838d7f9b7fc1820060c5d
bkhonye@gmail.com <bkhonye@gmail.com>
2026-01-23 04:51:18 +0200

    the code base structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+
+	images: {
+		qualities: [25, 50, 75],
+	},
 };
 
 export default nextConfig;
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import Logo from './benji-logo.png'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Image 
+        src= {Logo}
+        alt="Benji Logo"
+        width={50}
+        height={50}
+        quality={100}
+        placeholder='blur'
+      />
+          <h1>Benji Help desk</h1>
+          <Link href="/">Dashboard</Link> 
+          <Link href="/orders">Orders</Link>
+        </nav>
+  )
+}
